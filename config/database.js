86f4735028bc13d83cf529f3a29f888f3cdfbac5
Frank Fan <cfan0047@student.monash.edu>
2026-08-09 17:04:10 +0800

    Support CA certificate via environment variable

--- config/database.js
+++ config/database.js
@@ -1,11 +1,4 @@
 const mysql = require("mysql2/promise");
-const fs = require("fs");
-const path = require("path");
-
-const caPath = path.resolve(process.env.DB_CA_PATH);
-
-console.log("CA path:", caPath);
-console.log("CA exists:", fs.existsSync(caPath));
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -15,8 +8,7 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
 
     ssl: {
-        ca: fs.readFileSync(caPath),
-        rejectUnauthorized: false
+        ca: process.env.DB_CA_CERT
     },
 
     waitForConnections: true,
